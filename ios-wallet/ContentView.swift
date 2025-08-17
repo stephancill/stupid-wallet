43a3b8d1c85f9fdf79e456fcb444e9ebe1ef1490
Stephan Cilliers <5469870+stephancill@users.noreply.github.com>
2025-08-17 13:54:31 +0200

    Initial Commit

---
+++ ios-wallet/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ios-wallet
+//
+//  Created by Stephan on 2025/08/17.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ios-wallet/ios_walletApp.swift
@@ -0,0 +1,17 @@
+//
+//  ios_walletApp.swift
+//  ios-wallet
+//
+//  Created by Stephan on 2025/08/17.
+//
+
+import SwiftUI
+
+@main
+struct ios_walletApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
